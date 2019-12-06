2e05a98c670615f5aa2fa9b5ae9f399465c38a31
kanukale, monika <kanukalemonika000@gmail.com>
2019-12-06 09:37:07 +0530

    web module

--- WebContent/js/script.js
+++ WebContent/js/script.js
@@ -1,1 +1,32 @@
-// Include truYum form validation functions here
+function validate()
+{
+	var a = document.forms["Form"]["txt"].value;
+	var b = document.forms["Form"]["price"].value;
+	var c = document.forms["Form"]["dol"].value;
+	if (a == "") {
+		alert("Title is required");
+		document.Form.txt.focus();
+		return false;
+	}
+	if ((a <= 2) || (a >= 65)) {
+		alert("Title should have 2 to 65 characters");
+		document.Form.txt.focus();
+		return false;
+	}
+	if (b == "") {
+		alert("Price is required");
+		document.Form.price.focus();
+		return false;
+	}
+	var r = /[^0-9]/
+	if (r.test(b)) {
+		alert("Price has to be a number");
+		document.Form.price.focus();
+		return false;
+	}
+	if (c == "") {
+		alert("Date of Launch is required");
+		document.Form.dol.focus();
+		return false;
+	}
+}
